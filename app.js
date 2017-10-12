4f69a68dbde7c25b270ca4dceb86acc79bc229d2
s-e-v <severino.gurashi@gmail.com>
2017-10-12 10:34:54 +0100

    Fuction refactored individually and accounting for more than 8 bits (>255 decimal).

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 $("#convert").on("click", function () {
     var number = document.getElementById("decimal").value;
+    document.getElementById("binary").value = convBin(number);
+});
+
+function convBin (number) {
     var afterDiv = number;
     var remainder = [];
-    for (i = 0; i < 8; i++) {
+    for (i = 0; Math.floor(afterDiv) != 0; i++) {
         remainder[i] = Math.floor(afterDiv % 2);
         afterDiv = afterDiv / 2;
+        console.log(afterDiv);
     };
-    document.getElementById("binary").value = remainder.reverse().join("");
-});
+    return remainder.reverse().join("");
+};
